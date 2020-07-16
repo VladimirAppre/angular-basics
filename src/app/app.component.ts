e9bde3edf54c5549a678dd2bcdd37831b69d54d0
Vladimir K <vladimir.appre@gmail.com>
2020-07-16 21:10:43 +0300

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,21 @@ import {Component} from '@angular/core';
 
 export class AppComponent {
 
-  toggle: any = false;
+  arr = [1, 1, 2, 3, 5, 8, 13];
+  objects = [
+    {title: 'Post1', author: 'vladimir', text: 'Hello',
+      comments: [
+        {name: 'Max', text: 'lorem_1'},
+        {name: 'Anya', text: 'lorem_2'},
+        {name: 'Petya', text: 'lorem_3'},
+      ]
+    },
+    {title: 'Post2', author: 'Max 2', text: 'Hello',
+      comments: [
+        {name: 'Max 2', text: 'lorem_1'},
+        {name: 'Anya 2', text: 'lorem_2'},
+        {name: 'Petya 2', text: 'lorem_3'},
+      ]
+    }
+  ]
 }
